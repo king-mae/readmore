0404788fa6cf5ff70b91cf6cf7f2ef8dbfee388d
MohammedElazab <you@example.com>
2023-11-08 09:01:30 +0200

    all books

--- src/components/AllBooks.jsx
+++ src/components/AllBooks.jsx
@@ -1,44 +1,20 @@
 import '../App.css'
 import book from '../assets/book.png'
+import { books } from '../data.js'
 
 function AllBooks() {
-    return(
+
+    return (
         <div className='container px-10 mx-auto py-7'>
             <h1 className='text-2xl font-bold pb-2.5'>
-                AllBooks
+                All Books
             </h1>
             <div className='flex space-x-5 overflow-x-scroll no-scrollbar'>
-                <div className='h-52 w-36 shrink-0'>
-                    <img src={book} alt="book" className='w-full h-full'/>
-                </div>
-                <div className='h-52 w-36 shrink-0'>
-                    <img src={book} alt="book" className='w-full h-full'/>
-                </div>
-                <div className='h-52 w-36 shrink-0'>
-                    <img src={book} alt="book" className='w-full h-full'/>
-                </div>
-                <div className='h-52 w-36 shrink-0'>
-                    <img src={book} alt="book" className='w-full h-full'/>
-                </div>
-                <div className='h-52 w-36 shrink-0'>
-                    <img src={book} alt="book" className='w-full h-full'/>
-                </div>
-                <div className='h-52 w-36 shrink-0'>
-                    <img src={book} alt="book" className='w-full h-full'/>
-                </div>
-                <div className='h-52 w-36 shrink-0'>
-                    <img src={book} alt="book" className='w-full h-full'/>
-                </div>
-                <div className='h-52 w-36 shrink-0'>
-                    <img src={book} alt="book" className='w-full h-full'/>
-                </div>
-                <div className='h-52 w-36 shrink-0'>
-                    <img src={book} alt="book" className='w-full h-full'/>
-                </div>
-                <div className='h-52 w-36 shrink-0'>
-                    <img src={book} alt="book" className='w-full h-full'/>
-                </div>
-                
+                {books.map((item, index) => (
+                    <div key={index} className=' h-52 w-36 shrink-0'>
+                        <img src={item.Image} alt="book" className='w-full h-full rounded-xl'/>
+                    </div>
+                ))}
             </div>
         </div>
     )
---
+++ src/data.js
@@ -0,0 +1,54 @@
+import book from './assets/book.png'
+export const books = [
+    {
+      Name: "Atomic Habits",
+      Image: "https://m.media-amazon.com/images/I/812eE1lO0dL._AC_UL320_.jpg",
+      Rating: 4.37,
+    },
+    {
+      Name: "The Subtle Art of Not Giving a F*ck",
+      Image: "https://m.media-amazon.com/images/I/71t4GuxLCuL._AC_UL320_.jpg",
+      Rating: 3.9,
+    },
+    {
+      Name: "Meditations",
+      Image: "https://m.media-amazon.com/images/I/817HOFZewXL._AC_UY218_.jpg",
+      Rating: 4.8,
+    },
+    {
+      Name: "Man's Search for Meaning",
+      Image: "https://m.media-amazon.com/images/I/518YMJcBVDL._AC_UY218_.jpg",
+      Rating: 4.7,
+    },
+    {
+      Name: "Think and Grow Rich",
+      Image: "https://m.media-amazon.com/images/I/71UypkUjStL._AC_UY218_.jpg",
+      Rating: 4.2,
+    },
+    {
+      Name: "Rich Dad Poor Dad",
+      Image: "https://m.media-amazon.com/images/I/81BE7eeKzAL._AC_UY218_.jpg",
+      Rating: 4.2,
+    },
+    {
+      Name: "7 Habits of Highly Effective People",
+      Image: "https://m.media-amazon.com/images/I/71oei0tAnzL._AC_UY218_.jpg",
+      Rating: 4.7,
+    },
+    {
+      Name: "How to Win Friends and Influence People",
+      Image: "https://m.media-amazon.com/images/I/71vK0WVQ4rL._AC_UY218_.jpg",
+      Rating: 4.3,
+    },
+    {
+      Name: "12 Rules for Life",
+      Image: "https://m.media-amazon.com/images/I/61BRxtp9qtL._AC_UY218_.jpg",
+      Rating: 4.1,
+    },
+    {
+      Name: "The Power of Now",
+      Image: "https://m.media-amazon.com/images/I/41FNCXDjE+L._AC_UY218_.jpg",
+      Rating: 4.7,
+    },
+  ];
+  
